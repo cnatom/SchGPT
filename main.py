f56f191a4da5818eb2994d1c1f3a949e46ad0c6a
CUMT-Atom <ccatom@88.com>
2024-10-23 14:21:36 +0800

    [refactor]整理代码

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 import os
-from typing import Dict, Any, List
+from typing import List
 
-from langchain.chains.retrieval import create_retrieval_chain
 from langchain_community.document_loaders import JSONLoader
-from langchain_core.callbacks import BaseCallbackHandler
 from langchain_core.documents import Document
-from langchain_core.messages import BaseMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.outputs import LLMResult
-from langchain_core.runnables import RunnablePassthrough
-from langchain_ollama import OllamaLLM
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_ollama import OllamaLLM
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from pydantic import BaseModel
 
 from chain_callback import ChainCallback
 from store.faiss import FAISSStore
-from pydantic import BaseModel, Field
 
 if __name__ == '__main__':
     llm = OllamaLLM(model="qwen2.5:7b-instruct")
